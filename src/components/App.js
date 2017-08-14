f06f69e07f3a8698f5752929d5ec506904454528
Orlando Lamerez <lamerezo@tandem.ly>
2017-08-14 14:19:06 -0500

    Remove unnecessary file, update route

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react'
 import { Route } from 'react-router'
 import Navigation from './Navigation'
 import LeftColumn from './LeftColumn'
+import Home from '../routes/Home'
 import Profile from '../routes/Profile'
 import Preferences from '../routes/Preferences'
 import '../styles/app.css'
 import '../styles/flex.css'
-import '../styles/spacing.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App container-fluid">
 				<Navigation />
-				<div className="row no-gutters p0 container-fluid flex-grow">
+				<div className="row no-gutters p-0 container-fluid flex-grow">
 					<LeftColumn />
 					<div className="col-sm-8">
-						<Route path="/" component={Profile} />
+						<Route path="/" component={Home} />
+						<Route path="/user/:id" component={Profile} />
 						<Route path="/preferences" component={Preferences} />
 					</div>
 				</div>
