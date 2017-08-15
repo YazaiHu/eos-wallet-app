3826266488a50ee9fca1ede6b70c952a7973e906
stubelshe <stu.belshe@tandem.ly>
2017-08-15 16:14:08 -0500

    Transaction formatting

--- src/routes/TransactionHistory.js
+++ src/routes/TransactionHistory.js
@@ -5,7 +5,18 @@ class TransactionHistory extends Component {
 	render() {
 		return (
 			<div>
-				<Transactions />	
+                <div className="container-full">
+                    <div className="row">
+                        <div className="col-12">
+                            <h2>Transaction History</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+				            <Transactions />
+                        </div>
+                    </div>
+                </div>
 			</div>
 		)
 	}
