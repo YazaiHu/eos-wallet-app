9476cafc26456a7a3710f407e7f79d6f3aa8a5b4
Orlando Lamerez <lamerezo@tandem.ly>
2017-08-16 08:53:16 -0500

    Changing icon markup

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 
-const Icon = ({ alt = "Image", url, size = '' }) => (
-	<figure>
-		<i
-			aria-label={alt}
-			className={`icon ${size}`}
-			style={{ backgroundImage: `url(${url})` }} />
-	</figure>
+const Icon = ({
+	className,
+	alt = "Image",
+	url,
+	size = '' }) => (
+	<i
+		aria-label={alt}
+		className={className}
+		style={{ backgroundImage: `url(${url})` }} />
 )
 
 export default Icon
