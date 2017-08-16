3f3a07fad19844725da74e3a055320bfdd83257b
stubelshe <stu.belshe@tandem.ly>
2017-08-16 09:58:55 -0500

    Changes to list styles

--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -4,21 +4,23 @@ import {List, Icon} from '../components'
 import TransactionsQuery from '../query/transactions'
 
 const Transaction = ({ date, sender, memo, amount }) => (
-	<div className="-list-item -transaction">
+	<div className="-list-item -transaction clearfix">
 		<div className="-transaction-left">
 			<div className="-transaction-date">
 				<div className="date-month">{date.month}</div>
 				<div className="date-day">{date.day}</div>
 			</div>
 
-			<Icon url={sender.icon} />
+			<Icon className="-thumbnail" url={sender.icon} />
 
-			<p className="-transaction-memo">{memo}</p>
+			<div className="-transaction-memo">
+                <p>{memo}</p>
+            </div>
 		</div>
 
 		<div className="-transaction-amount-container">
 			<p className="-transaction-amount">{amount}</p>
-			<Icon />
+			<Icon className="-transaction-icon" />
 		</div>
 	</div>
 )
