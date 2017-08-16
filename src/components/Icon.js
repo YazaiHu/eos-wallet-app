a67dc75d57a4b1640642a844f7750ac19c4d1da1
Orlando Lamerez <lamerezo@tandem.ly>
2017-08-16 08:55:54 -0500

    Add <div /> wrapper

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,11 +4,15 @@ const Icon = ({
 	className,
 	alt = "Image",
 	url,
-	size = '' }) => (
-	<i
-		aria-label={alt}
-		className={className}
-		style={{ backgroundImage: `url(${url})` }} />
+	style = {
+		icon: '-icon-class'
+	}}) => (
+	<div className={className}>
+		<i
+			aria-label={alt}
+			className={style.icon}
+			style={{ backgroundImage: `url(${url})` }} />
+	</div>
 )
 
 export default Icon
